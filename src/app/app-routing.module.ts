513e431116a9aa0646a969eca7abed23b263a1f4
Naqiah <22ftt1495@student.pb.edu.bn>
2024-04-05 17:52:09 +0800

    Update routing and page for front page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./front-page/front-page.module').then( m => m.HomePageModule)
   },
   {
     path: 'getting-started',
